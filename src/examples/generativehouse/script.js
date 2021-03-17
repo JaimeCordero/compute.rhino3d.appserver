8591d17ea154be2400070c62bcbdf1c0454438e3
JaimeCordero <77294916+JaimeCordero@users.noreply.github.com>
2021-03-17 21:02:58 +0100

    Fix download button

--- src/examples/generativehouse/script.js
+++ src/examples/generativehouse/script.js
@@ -410,7 +410,7 @@ function onWindowResize() {
     const blob = new Blob([bytes], {type: "application/octect-stream"})
 
     // use "hidden link" trick to get the browser to download the blob
-    const filename = data.definition.replace(/\.gh$/, '') + '.3dm'
+    const filename = definition.replace(/\.gh$/, '') + '.3dm'
     const link = document.createElement('a')
     link.href = window.URL.createObjectURL(blob)
     link.download = filename
