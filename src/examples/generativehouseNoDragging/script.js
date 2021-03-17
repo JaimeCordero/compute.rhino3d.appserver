1aa69b61ba542454a55e20c0801c2863282f3df6
JaimeCordero <77294916+JaimeCordero@users.noreply.github.com>
2021-03-17 01:45:46 +0100

    Final assignment updated
    @fraguada and @pearswj the fully working example is generativehouse

--- src/examples/generativehouseNoDragging/script.js
+++ src/examples/generativehouseNoDragging/script.js
@@ -10,54 +10,10 @@ loader.setLibraryPath( 'https://cdn.jsdelivr.net/npm/rhino3dm@0.15.0-beta/' )
 
 // initialise 'data' object that will be used by compute()
 const definition = 'generativehouse.gh';
-/*const data = {
+const data = {
   definition: 'generativehouse.gh',
   inputs: getInputs()
-}*/
-
-const slope = document.getElementById( 'RH_IN:slope' )
-slope.addEventListener( 'mouseup', onSliderChange, false )
-slope.addEventListener( 'touchend', onSliderChange, false )
-const height = document.getElementById( 'RH_IN:height' )
-height.addEventListener( 'mouseup', onSliderChange, false )
-height.addEventListener( 'touchend', onSliderChange, false )
-const houseDepth = document.getElementById( 'RH_IN:houseDepth' )
-houseDepth.addEventListener( 'mouseup', onSliderChange, false )
-houseDepth.addEventListener( 'touchend', onSliderChange, false )
-const houseWidth = document.getElementById( 'RH_IN:houseWidth' )
-houseWidth.addEventListener( 'mouseup', onSliderChange, false )
-houseWidth.addEventListener( 'touchend', onSliderChange, false )
-const count = document.getElementById( 'RH_IN:count' )
-count.addEventListener( 'mouseup', onSliderChange, false )
-count.addEventListener( 'touchend', onSliderChange, false )
-const generator = document.getElementById( 'RH_IN:generator' )
-generator.addEventListener( 'mouseup', onSliderChange, false )
-generator.addEventListener( 'touchend', onSliderChange, false )
-const location = document.getElementById( 'RH_IN:location' )
-location.addEventListener( 'mouseup', onSliderChange, false )
-location.addEventListener( 'touchend', onSliderChange, false )
-const windowCount = document.getElementById( 'RH_IN:windowCount' )
-windowCount.addEventListener( 'mouseup', onSliderChange, false )
-windowCount.addEventListener( 'touchend', onSliderChange, false )
-const sillHeight = document.getElementById( 'RH_IN:sillHeight' )
-sillHeight.addEventListener( 'mouseup', onSliderChange, false )
-sillHeight.addEventListener( 'touchend', onSliderChange, false )
-const windowHeight = document.getElementById( 'RH_IN:windowHeight' )
-windowHeight.addEventListener( 'mouseup', onSliderChange, false )
-windowHeight.addEventListener( 'touchend', onSliderChange, false )
-const windowWidth = document.getElementById( 'RH_IN:windowWidth' )
-windowWidth.addEventListener( 'mouseup', onSliderChange, false )
-windowWidth.addEventListener( 'touchend', onSliderChange, false )
-const poolLarge = document.getElementById( 'RH_IN:poolLarge' )
-poolLarge.addEventListener( 'mouseup', onSliderChange, false )
-poolLarge.addEventListener( 'touchend', onSliderChange, false )
-const poolWidth = document.getElementById( 'RH_IN:poolWidth' )
-poolWidth.addEventListener( 'mouseup', onSliderChange, false )
-poolWidth.addEventListener( 'touchend', onSliderChange, false )
-
-
-const showRoof = document.querySelector('input[id="RH_IN:showRoof"]');
-showRoof.addEventListener( 'change', onSliderChange, false )
+}
 
 let points = []
 
@@ -68,7 +24,7 @@ rhino3dm().then(async m => {
     rhino = m
 
     init()
-    initialPts()
+    //initialPts()
     compute()
 })
 
@@ -82,7 +38,7 @@ downloadButton.onclick = download
 /**
  * Gets <input> elements from html and sets handlers
  * (html is generated from the grasshopper definition)
- 
+ */
 function getInputs() {
   const inputs = {}
   for (const input of document.getElementsByTagName('input')) {
@@ -104,12 +60,10 @@ function getInputs() {
         break
     }
   }
-
-
   return inputs
-}*/
+}
 
-function initialPts() {
+/*function initialPts() {
 
   const startPts = [
     { x: 0, y: 0, z: 0 },
@@ -170,31 +124,15 @@ function onChange() {
 
   controls.enabled = false
 
-}
+}*/
 
 /**
  * Call appserver
  */
  async function compute () {
-
   const data = {
     definition: definition,
     inputs: {
-      'RH_IN:slope': slope.valueAsNumber,
-      'RH_IN:height': height.valueAsNumber,
-      'RH_IN:houseDepth': houseDepth.valueAsNumber,
-      'RH_IN:count': count.valueAsNumber,
-      'RH_IN:generator': generator.valueAsNumber,
-      'RH_IN:location': location.valueAsNumber,
-      'RH_IN:windowCount': windowCount.valueAsNumber,
-      'RH_IN:sillHeight': sillHeight.valueAsNumber,
-      'RH_IN:windowHeight': windowHeight.valueAsNumber,
-      'RH_IN:windowWidth': windowWidth.valueAsNumber,
-      'RH_IN:poolLarge': poolLarge.valueAsNumber,
-      'RH_IN:poolWidth': poolWidth.valueAsNumber,
-
-      'RH_IN:showRoof': showRoof.checked,
-
       'points': points
     }
   }
@@ -226,54 +164,49 @@ function onChange() {
 /**
  * Parse response
  */
- function collectResults(responseJson) {
+function collectResults(responseJson) {
 
-  const values = responseJson.values
+    const values = responseJson.values
 
-  console.log(values)
-
-  // clear doc
-  try {
+    // clear doc
     if( doc !== undefined)
         doc.delete()
-  } catch {}
 
-  //console.log(values)
-  doc = new rhino.File3dm()
+    //console.log(values)
+    doc = new rhino.File3dm()
 
-  // for each output (RH_OUT:*)...
-  for ( let i = 0; i < values.length; i ++ ) {
-    // ...iterate through data tree structure...
-    for (const path in values[i].InnerTree) {
-      const branch = values[i].InnerTree[path]
-      // ...and for each branch...
-      for( let j = 0; j < branch.length; j ++) {
-        // ...load rhino geometry into doc
-        const rhinoObject = decodeItem(branch[j])
-        if (rhinoObject !== null) {
-          // console.log(rhinoObject)
-          doc.objects().add(rhinoObject, null)
+    // for each output (RH_OUT:*)...
+    for ( let i = 0; i < values.length; i ++ ) {
+      // ...iterate through data tree structure...
+      for (const path in values[i].InnerTree) {
+        const branch = values[i].InnerTree[path]
+        // ...and for each branch...
+        for( let j = 0; j < branch.length; j ++) {
+          // ...load rhino geometry into doc
+          const rhinoObject = decodeItem(branch[j])
+          if (rhinoObject !== null) {
+            doc.objects().add(rhinoObject, null)
+          }
         }
       }
     }
-  }
 
-  if (doc.objects().count < 1) {
-    console.error('No rhino objects to load!')
-    showSpinner(false)
-    return
-  }
+    if (doc.objects().count < 1) {
+      console.error('No rhino objects to load!')
+      showSpinner(false)
+      return
+    }
 
-  // hack (https://github.com/mcneel/rhino3dm/issues/353)
-  const sphereAttrs = new rhino.ObjectAttributes()
-  sphereAttrs.mode = rhino.ObjectMode.Hidden
-  doc.objects().addSphere(new rhino.Sphere([0,0,0], 0.001), sphereAttrs)
+    // hack (https://github.com/mcneel/rhino3dm/issues/353)
+    const sphereAttrs = new rhino.ObjectAttributes()
+    sphereAttrs.mode = rhino.ObjectMode.Hidden
+    doc.objects().addSphere(new rhino.Sphere([0,0,0], 0.001), sphereAttrs)
 
-  // load rhino doc into three.js scene
-  const buffer = new Uint8Array(doc.toByteArray()).buffer
-  loader.parse( buffer, function ( object ) 
-  {
-            // debug 
+    // load rhino doc into three.js scene
+    const buffer = new Uint8Array(doc.toByteArray()).buffer
+    loader.parse( buffer, function ( object ) 
+    {
+        // debug 
         /*
         object.traverse(child => {
           if (child.material !== undefined)
@@ -281,49 +214,49 @@ function onChange() {
         }, false)
         */
 
-      // clear objects from scene
-      scene.traverse(child => {
-        if ( child.userData.hasOwnProperty( 'objectType' ) && child.userData.objectType === 'File3dm') {
-          scene.remove( child )
-        }
-      })
+        // clear objects from scene. do this here to avoid blink
+        scene.traverse(child => {
+            if (!child.isLight) {
+                scene.remove(child)
+            }
+        })
 
-            // add object graph from rhino model to three.js scene
-      scene.add( object )
+        // add object graph from rhino model to three.js scene
+        scene.add( object )
 
-      // hide spinner and enable download button
-      showSpinner(false)
-      downloadButton.disabled = false
+        // hide spinner and enable download button
+        showSpinner(false)
+        downloadButton.disabled = false
 
-      /* zoom to extents*/
-      //  zoomCameraToSelection(camera, controls, scene.children)
-  })
+        /* zoom to extents*/
+        //zoomCameraToSelection(camera, controls, scene.children)
+    })
 }
 
 /**
-* Attempt to decode data tree item to rhino geometry
-*/
+ * Attempt to decode data tree item to rhino geometry
+ */
 function decodeItem(item) {
-const data = JSON.parse(item.data)
-if (item.type === 'System.String') {
-  // hack for draco meshes
-  try {
-      return rhino.DracoCompression.decompressBase64String(data)
-  } catch {} // ignore errors (maybe the string was just a string...)
-} else if (typeof data === 'object') {
-  return rhino.CommonObject.decode(data)
-}
-return null
+  const data = JSON.parse(item.data)
+  if (item.type === 'System.String') {
+    // hack for draco meshes
+    try {
+        return rhino.DracoCompression.decompressBase64String(data)
+    } catch {} // ignore errors (maybe the string was just a string...)
+  } else if (typeof data === 'object') {
+    return rhino.CommonObject.decode(data)
+  }
+  return null
 }
 
 /**
  * Called when a slider value changes in the UI. Collect all of the
  * slider values and call compute to solve for a new scene
  */
- function onSliderChange () {
+function onSliderChange () {
   showSpinner(true)
   // get slider values
-  /*let inputs = {}
+  let inputs = {}
   for (const input of document.getElementsByTagName('input')) {
     switch (input.type) {
     case 'number':
@@ -337,8 +270,8 @@ return null
       break
     }
   }
-    
-  data.inputs = inputs*/
+  
+  data.inputs = inputs
 
   compute()
 }
@@ -346,7 +279,7 @@ return null
 /**
  * The animation loop!
  */
- function animate() {
+function animate() {
   requestAnimationFrame( animate )
   controls.update()
   renderer.render(scene, camera)
@@ -360,44 +293,47 @@ let scene, camera, renderer, controls
  */
 function init() {
 
-  // Rhino models are z-up, so set this as the default
-  THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
+    // Rhino models are z-up, so set this as the default
+    THREE.Object3D.DefaultUp = new THREE.Vector3( 0, 0, 1 );
 
-  // create a scene and a camera
-  scene = new THREE.Scene()
-  scene.background = new THREE.Color(0xf7fbfd)
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
-  camera.position.set(-10, -10, 15) // like perspective view
+    // create a scene and a camera
+    scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xf7fbfd)
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.set(-12, -8, 15) // like perspective view
 
-  // very light grey for background, like rhino
-  //scene.background = new THREE.Color('whitesmoke')
+    // very light grey for background, like rhino
+    //scene.background = new THREE.Color('whitesmoke')
 
-  // create the renderer and add it to the html
-  renderer = new THREE.WebGLRenderer({ antialias: true })
-  renderer.setPixelRatio( window.devicePixelRatio )
-  renderer.setSize(window.innerWidth, window.innerHeight)
-  document.body.appendChild(renderer.domElement)
+    // create the renderer and add it to the html
+    renderer = new THREE.WebGLRenderer({ antialias: true })
+    renderer.setPixelRatio( window.devicePixelRatio )
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
 
-  // add some controls to orbit the camera
-  controls = new OrbitControls(camera, renderer.domElement)
-  controls.target.set(10,10,-5);
-  controls.update();
+    // add some controls to orbit the camera
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.target.set(8,12,-5);
+    controls.update();
 
-  // add a directional light
-  const directionalLight = new THREE.DirectionalLight( 0xffffff )
-  directionalLight.intensity = 2
-  directionalLight.position.set(10,-10,5)
-  scene.add( directionalLight )
+    // add a directional light
+    const directionalLight = new THREE.DirectionalLight( 0xFFF8E4 )
+    directionalLight.intensity = 2
+    directionalLight.position.set(-10,-10,15)
+    scene.add( directionalLight )
 
-  const ambientLight = new THREE.AmbientLight()
-  scene.add( ambientLight )
+    const ambientLight = new THREE.AmbientLight()
+    scene.add( ambientLight )
 
-  // handle changes in the window size
-  window.addEventListener( 'resize', onWindowResize, false )
+    // handle changes in the window size
+    window.addEventListener( 'resize', onWindowResize, false )
 
-  animate()
+    animate()
 }
 
+/**
+ * Helper function for window resizes (resets the camera pov and renderer size)
+  */
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
@@ -408,7 +344,7 @@ function onWindowResize() {
 /**
  * This function is called when the download button is clicked
  */
- function download () {
+function download () {
     // write rhino doc to "blob"
     const bytes = doc.toByteArray()
     const blob = new Blob([bytes], {type: "application/octect-stream"})
@@ -424,7 +360,7 @@ function onWindowResize() {
 /**
  * Shows or hides the loading spinner
  */
- function showSpinner(enable) {
+function showSpinner(enable) {
   if (enable)
     document.getElementById('loader').style.display = 'block'
   else
